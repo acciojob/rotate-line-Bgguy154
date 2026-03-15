81d2d64d3aa494aae0afcd4d070f6320b5ee1fea
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-03-15 15:08:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const div=document.querySelector("div");
+
+let angle=0;
+
+setInterval(()=>{
+	angle+=2;
+
+	div.style.transform=`translate(-50%,50%) rotate(${angle}deg)`
+},20)
